3ea4e631947dde1eff103301c5f89095c71de994
Ivan Ivashchenko <ivan86.ivashchenko@gmail.com>
2023-03-26 12:33:27 +0300

    Add function greeting at /src/cli.js & export it in brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+
+import userName from '../src/cli.js';
+
+console.log(`Hello, ${userName}!`);
---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from "readline-sync";
+
+console.log('Welcome to the Brain Games!');
+
+let userName = readlineSync.question('May I have your name? ');
+
+export default userName;
