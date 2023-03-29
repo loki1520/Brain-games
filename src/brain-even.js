c0c2172b6e1843cb70c0106abd5230028f06cea6
Ivan Ivashchenko <ivan86.ivashchenko@gmail.com>
2023-03-29 19:40:00 +0300

    Took the constant out at src/brain-even.js

--- src/brain-even.js
+++ src/brain-even.js
@@ -1,11 +1,11 @@
 import readlineSync from 'readline-sync';
 
+const numberOfRound = 3;
 const brainEven = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const numberOfRound = 3;
   for (let i = 0; i < numberOfRound; i += 1) {
     const getRandomNumber = () => Math.floor(Math.random() * 1000);
     const randomNumber = getRandomNumber();
@@ -13,11 +13,11 @@ const brainEven = () => {
     console.log(`Question: ${randomNumber}`);
     const answer = readlineSync.question('Your answer: ');
 
-    if (answer !== 'yes' && answer !== 'no') {
-      console.log(`"${answer}" is wrong answer ;(.`);
-      console.log(`Let's try again, ${userName}!`);
-      return false;
-    }
+    // if (answer !== 'yes' && answer !== 'no') {
+    //   console.log(`"${answer}" is wrong answer ;(.`);
+    //   console.log(`Let's try again, ${userName}!`);
+    //   return false;
+    // }
 
     if (answer === 'yes' && randomNumber % 2 !== 0) {
       console.log('"yes" is wrong answer ;(. Correct answer was "no".');
